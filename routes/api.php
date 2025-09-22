35ee8493dfd0192bee03cd55b7795829658b48ed
toiletmonk <dispatchwarriors076@gmail.com>
2025-09-22 13:42:47 -0500

    Minor updates

--- routes/api.php
+++ routes/api.php
@@ -4,12 +4,15 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\DownloadController;
 use App\Http\Controllers\ForgotPasswordController;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\StripePaymentController;
+use App\Http\Controllers\StripeWebhookController;
 use App\Http\Controllers\UploadController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
-Route::post('forgot-passowrd', [ForgotPasswordController::class, 'sendResetLinkEmail'])->name('password.email');
+Route::post('forgot-password', [ForgotPasswordController::class, 'sendResetLinkEmail'])->name('password.email');
+Route::post('/stripe/webhook', [StripeWebhookController::class, 'handleWebhook']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::put('changePassword', [AuthController::class, 'changePassword']);
@@ -21,4 +24,6 @@ Route::middleware('auth:sanctum')->group(function () {
 
     Route::get('posts/download/{id}', [DownloadController::class, 'download']);
     Route::post('upload', [UploadController::class, 'upload']);
+
+    Route::post('/stripe/create-payment-intent', [StripePaymentController::class, 'createPaymentIntent']);
 });
